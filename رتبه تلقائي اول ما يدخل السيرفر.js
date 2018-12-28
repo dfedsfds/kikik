bf8c2777c5cc5df0285a95e0570f4fbb1c8367cf
dfedsfds <45593336+dfedsfds@users.noreply.github.com>
2018-12-28 14:22:47 +0300

    Update رتبه تلقائي اول ما يدخل السيرفر.js

--- رتبه تلقائي اول ما يدخل السيرفر.js
+++ رتبه تلقائي اول ما يدخل السيرفر.js
@@ -1,3 +1,7 @@
 client.on('guildMemberAdd', member=> {
-    member.addRole(member.guild.roles.find("name","اسم الرتبه"));
+    member.addRole(member.guild.roles.find("name","SV."));
     });
+
+
+
+client.login(process.env.BOT_TOKEN);
